81038d7e86796ae03ab97a93c8ebe6b62836b873
akame <87499021+ckbi@users.noreply.github.com>
2023-08-07 11:47:04 +0200

    Update non1usdcvv.php

--- gate/non1usdcvv.php
+++ gate/non1usdcvv.php
@@ -33,7 +33,7 @@ if(empty($amt)) {
 	$amt = '1';
 	$chr = $amt * 100;
 }
-$sk = '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';
+$sk = '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';
 
 $lista = $_GET['lista'];
     $cc = multiexplode(array(":", "|", ""), $lista)[0];
